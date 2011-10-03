46821e662aa6fde589c7c1e1071fc4d372676744
Victorio Iglesias Gutiérrez-Cecchini <viglesias@neosystems.es>
2011-10-04 01:37:30 +0200

    Make all Till page 111 from pdf

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -22,6 +22,16 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
   end
 end
 
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
+
 Then /^(?:|I )should not see "([^"]*)"$/ do |text|
   if page.respond_to? :should
     page.should have_no_content(text)
