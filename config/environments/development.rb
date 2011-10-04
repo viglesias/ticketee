53e9afe811a439afc49fdf1e24b69ea8c44be6cd
Victorio Iglesias Gutiérrez-Cecchini <viglesias@neosystems.es>
2011-10-04 22:47:41 +0200

    Added feature to ensure Devise signup is always working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Ticketee::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -27,4 +27,8 @@ Ticketee::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Email configuration for development environment
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+
 end
