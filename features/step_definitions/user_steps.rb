e084d21ac88b045f54d5d5c8aea26987475a33b5
Victorio Iglesias Gutiérrez-Cecchini <viglesias@neosystems.es>
2011-10-05 22:06:56 +0200

    Lock down specific projects controller actions for admins only

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,9 +2,11 @@ Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
     unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.update_attribute("admin", attributes["admin"] == "true")
     @user.confirm! unless unconfirmed
   end
 end
+
 
 Given /^I am signed in as them$/ do
   steps(%Q{
@@ -15,5 +17,10 @@ And I fill in "Password" with "password"
 And I press "Sign in"
 Then I should see "Signed in successfully."
 })
+end
+
+Given /^I am signed in as "([^\"]*)"$/ do |email|
+  @user = User.find_by_email!(email)
+  steps("Given I am signed in as them")
 end
 
